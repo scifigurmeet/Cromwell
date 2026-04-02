c436e81f8be8a792a2e64a66eabae1bf2c181168
scifigurmeet <56145753+scifigurmeet@users.noreply.github.com>
2026-04-02 14:16:58 +0530

    Add comment to CContainer.tsx
    Added a comment to trigger the pipeline.

--- system/core/frontend/src/components/CContainer/CContainer.tsx
+++ system/core/frontend/src/components/CContainer/CContainer.tsx
@@ -7,6 +7,7 @@ export type CContainerProps = {
   children?: React.ReactNode;
 } & TCromwellBlockProps;
 
+//This is just some change to trigger pipeline
 export class CContainer extends React.Component<CContainerProps> {
   render() {
     const { children, ...rest } = this.props;
